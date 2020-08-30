587f8d87199094dd1c234f7d989e80ce0dc2b1a4
Bryan Selner <dev@recoilvelocity.com>
2020-08-29 20:11:23 -0700

    Added configuring github ssh for access to private repositories

--- github.sh
+++ github.sh
@@ -1,14 +1,47 @@
 #!/bin/sh
 
 echo "Configuring Github and cloning repositories..."
+
 #---------------------------
 #  LOAD ENV VARIABLES
 #---------------------------
 
 export $(grep -v '^#' .env | xargs)
 
+
+#---------------------------
+#  CONFIGURE GITHUB SSH
+#---------------------------
+
+if [ -z ${GITHUB_USER+x} ] || [ -z ${GITHUB_KEYFILE+x} ]; then
+  echo "ERROR!   GITHUB_KEYFILE AND GITHUB_USER environment variables are not set.  Aborting!"
+  exit 1
+else
+  echo "... adding github ssh values to .ssh/config"
+  echo "" >>"$HOME/.ssh/config"
+  echo "" >>"$HOME/.ssh/config"
+  echo "##############################################################" >>"$HOME/.ssh/config"
+  echo "#       ADDED BY DOTFILES SETUP - $(date +'%Y%m%d_%H%M_%S')           #" >>"$HOME/.ssh/config"
+  echo "##############################################################" >>"$HOME/.ssh/config"
+  echo "Host $GITHUB_USER" >>"$HOME/.ssh/config"
+  echo "HostName GitHub" >>"$HOME/.ssh/config"
+  echo "PreferredAuthentications publickey" >>"$HOME/.ssh/config"
+  echo "IdentityFile $GITHUB_KEYFILE" >>"$HOME/.ssh/config"
+  echo "##############################################################" >>"$HOME/.ssh/config"
+fi
+
+echo "... testing github connection"
+ssh -T git@github.com
+
+
+#---------------------------
+#  CLONE REPOSITORIES
+#---------------------------
+
 CODE=$HOME/Dropbox/code
 mkdir -pv "$CODE"
+
+cd "$CODE"
 
 # SITES=$HOME/Sites
 # BLADE=$SITES/blade-ui-kit
